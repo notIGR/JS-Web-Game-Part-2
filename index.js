cb899a6206dcbff7b35eaee94af7fdd5bdc54c1e
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-20 20:03:48 -0400

    fixed scope of inventory

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function newItem(url, left, bottom) {
   });
 }
 
+let inventory = document.createElement("div");
 function newInventory() {
-  let inventory = document.createElement("div");
   inventory.style.position = "fixed";
   inventory.style.bottom = "0px";
   inventory.style.left = "0px";
